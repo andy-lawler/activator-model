77a02c02419b20f4a14ba42c6521d07c16b25cfa
Andy <36938428+andy-lawler@users.noreply.github.com>
2020-12-04 16:33:12 -0500

    Update Process.sh

--- Process.sh
+++ Process.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
-
 set -e
-
  
-
 dos2unix *.py
-
 chmod a+x *.py
-
  
-
 chmod a+x *.sql
-
 chmod a+x *.sh
-
 dos2unix *
-
  
-
 echo "Cleaning Data"
-
 ./Clean_Data.py
-
  
-
 echo "Process Complete"
