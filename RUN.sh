6efd97ff3d8f24517754ccc3146e19f69305b2b5
Andy <36938428+andy-lawler@users.noreply.github.com>
2020-12-04 16:33:52 -0500

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-
 set -e
-
  
-
 echo "GENERATING ACTIVATION DATASET"
-
 dos2unix *.sql
-
  
-
 impala="/usr/bin/impala-shell -i "
-
 echo "Downloading the training data to csv"
-
  
-
 OUTPUT="activation_data_mm.csv"
-
 $impala -f "query_activation_data.sql" -B -o "$OUTPUT" --output_delimiter=',' --print_header
-
  
-
 echo "Process Complete"
